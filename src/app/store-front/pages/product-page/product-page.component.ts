999a5df95a849fb190d3704ca1590498cd0f5e65
JARA-17 <juandres435@gmail.com>
2026-02-11 18:44:06 +0100

    feat: Integrates product carousel with Swiper and dynamic loading
    Enhances product page by adding carousel support for image display
    using Swiper, improves user experience with loading states, and
    retrieves product data dynamically based on route parameters.

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@/products/services/products.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductCarouselComponent } from "@/products/components/product-carousel/product-carousel.component";
 @Component({
   selector: 'product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
-  styles: ``
 })
 export default class ProductPageComponent {
+  private productsService = inject(ProductsService);
+  private activatedRoute = inject(ActivatedRoute);
+
+  idSlug = computed(() => {
+    return this.activatedRoute.snapshot.paramMap.get('idSlug');
+  });
+
+  productResource = rxResource({
+    request: () => ({
+      slug: this.idSlug()!,
+    }),
+
+    loader: ({ request }) => {
+      return this.productsService.getProductBySlug(request.slug);
+    }
+  });
 
 }
